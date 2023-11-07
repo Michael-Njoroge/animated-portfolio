f556b8ab2644e92e99b152a97c2a5cf89b158a07
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-07 21:18:36 +0300

    initialize constants in js

---
+++ js/animation.js
@@ -0,0 +1,28 @@
+// Define DOM elements
+const heroImage = document.querySelector("#hero_animation_img");
+
+const tl = document.querySelector("#grid_tl");
+const tr = document.querySelector("#grid_tr");
+const bl = document.querySelector("#grid_bl");
+const br = document.querySelector("#grid_br");
+
+const tlBtn = document.querySelector("#grid_tl_btn");
+const trBtn = document.querySelector("#grid_tr_btn");
+const blBtn = document.querySelector("#grid_bl_btn");
+const brBtn = document.querySelector("#grid_br_btn");
+
+const tlContent = document.querySelector("#grid_tl_Content");
+const trContent = document.querySelector("#grid_tr_Content");
+const blContent = document.querySelector("#grid_bl_Content");
+const brContent = document.querySelector("#grid_br_Content");
+
+const projectOne = document.querySelector(".p-1");
+const projectTwo = document.querySelector(".p-2");
+const projectThree = document.querySelector(".p-3");
+
+// Define colors and positions
+const bgColor = "var(--bg)";
+const bgColorAlt = "var(--bg-alt)";
+const textColor = "var(--text)";
+const textColorAlt = "var(--text-alt)";
+
